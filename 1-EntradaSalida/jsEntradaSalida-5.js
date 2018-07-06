e546807534b9ab4a342bd58d5b05c699fd69b747
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-06 19:03:59 -0300

    Terminados
    realizados en clase

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,10 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+	var age;
+	var name;
+	name=document.getElementById('elNombre').value;
+	age=document.getElementById('laEdad').value;
+	alert("Usted se llama "+name+" y su tiene "+age+" años");
 }
 
