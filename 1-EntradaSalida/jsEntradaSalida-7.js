6168bf389e59369cdfb08f2035f2f5446d4937fc
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-06 19:10:39 -0300

    el 7 resuelto
    resuelto en clase

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,53 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno=document.getElementById('numeroUno').value;
+	numerodos=document.getElementById('numeroDos').value;
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+	resultado=numerouno + numerodos;
+	alert("el resultado de la suma de los numeros es : "+resultado);
 }
 
 function restar()
 {
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno=document.getElementById('numeroUno').value;
+	numerodos=document.getElementById('numeroDos').value;
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+	resultado=numerouno - numerodos;
+	alert("el resultado de la resta de los numeros es : "+resultado);
 }
 
 function multiplicar()
 { 
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno=document.getElementById('numeroUno').value;
+	numerodos=document.getElementById('numeroDos').value;
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+	resultado=numerouno * numerodos;
+	alert("el resultado de la multiplicacion de los numeros es : "+resultado);
 }
 
 function dividir()
 {
-	
+	var numerouno;
+	var numerodos;
+	var resultado;
+	numerouno=document.getElementById('numeroUno').value;
+	numerodos=document.getElementById('numeroDos').value;
+	numerouno=parseInt(numerouno);
+	numerodos=parseInt(numerodos);
+	resultado=numerouno / numerodos;
+	alert("el resultado de la division de los numeros es : "+resultado);
 }
 
