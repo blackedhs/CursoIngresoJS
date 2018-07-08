c818a098c3f5b9ea5eebad6d4b0aed4d142014dc
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-08 15:40:24 -0300

    Ferrete Facturacion
    Resuelto en casa

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,59 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+	precioTres=document.getElementById('PrecioTres').value;
+	precioTres=parseInt(precioTres);
+
+	suma=precioUno+precioDos+precioTres;
+
+	alert("La suma de los precios es: "+suma);
+
+
 }
 function Promedio () 
 {
-	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var promedio;	
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+	precioTres=document.getElementById('PrecioTres').value;
+	precioTres=parseInt(precioTres);
+
+	promedio=(precioUno+precioDos+precioTres)/3;
+
+	alert("El promedio de los precios es: "+promedio);
 }
 function PrecioFinal () 
 {
-	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+	var precioConIva;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioUno=parseInt(precioUno);
+	precioDos=document.getElementById('PrecioDos').value;
+	precioDos=parseInt(precioDos);
+	precioTres=document.getElementById('PrecioTres').value;
+	precioTres=parseInt(precioTres);
+
+	suma=precioUno+precioDos+precioTres;
+	precioConIva=suma*1.21;
+
+	alert("El precio final + iva es de: "+precioConIva);
 }
