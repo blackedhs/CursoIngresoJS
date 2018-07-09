3f61f670ef9e44dff50623a67c6258f705960d52
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-09 18:04:20 -0300

    Reflejos daltonicos 1
    resuelto en casa

--- 8-TPs/jsReflejosDaltonicos1.js
+++ 8-TPs/jsReflejosDaltonicos1.js
@@ -6,16 +6,47 @@ mostrara el texto de un color entre los 6 posibles
 */
 var ColorSecreto;
 var tiempoInicio;
+
+setInterval(tiempo,1000);
+function tiempo()
+{
+	tiempoInicio++;
+}
 function comenzar()
 {
-
+	tiempoInicio=0;
+	ColorSecreto=Math.random();
+	ColorSecreto*=6;
+	ColorSecreto=Math.ceil(ColorSecreto);
+	switch (ColorSecreto){
+		case 1:
+			ColorSecreto="azul";
+			break;
+		case 2:
+			ColorSecreto="amarillo";
+			break;
+		case 3:
+			ColorSecreto="marron";
+			break;
+		case 4:
+			ColorSecreto="verde";
+			break;
+		case 5:
+			ColorSecreto="celeste";
+			break;
+		case 6:
+			ColorSecreto="rojo";
+	}
+	document.getElementById('ColorElejido').value=ColorSecreto;
 	
 
 }//FIN DE LA FUNCIÓN
 function Responder(colorParametro)
 {
-	
-	
-
-
+	if (colorParametro==ColorSecreto){
+		alert("Correcto!!!.Tu tiempo de respuesta fue de :"+tiempoInicio+" segundos");
+	}else{
+		alert("Incorrecto. Vuelva a intentarlo");
+	}
+	comenzar();
 }//FIN DE LA FUNCIÓN
