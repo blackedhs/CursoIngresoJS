980405708672783b754da412b73e09ac5bca9074
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-10 21:56:49 -0300

    resuelto en clase
    Resuelto

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,17 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
 
+	edad=document.getElementById('edad').value; 
+
+	/*if(13<=edad&&edad<=17){
+		alert("Usted es adolescente");
+	}*/
+	if (edad<=17) {
+		if (edad>=13) {
+			alert("Usted es adolescente");
+		}
+	}
 
 
 }//FIN DE LA FUNCIÓN
