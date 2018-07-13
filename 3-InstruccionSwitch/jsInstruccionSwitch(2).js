cbcb1525ecdd031c57a723b5e40b9728eb42091e
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-13 19:58:13 -0300

    todos resueltos
    En clase!!!

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,46 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	switch(mesDelAño){
+		case "Enero":
+			alert("Ya paso el frio, ahora calor!!");
+			break;
+		case "Febrero":
+			alert("Ya paso el frio, ahora calor!!");
+			break;
+		case "Marzo":
+			alert("Falta para el invierno");
+			break;
+		case "Abril":
+			alert("Falta para el invierno");
+			break;
+		case "Mayo":
+			alert("Falta para el invierno");
+			break;
+		case "Junio":
+			alert("Abrigate que hace frio");
+			break;
+		case "Julio":
+			alert("Abrigate que hace frio");
+			break;
+		case "Agosto":
+			alert("Abrigate que hace frio");
+			break;
+		case "Septiembre":
+			alert("Ya paso el frio, ahora calor!!");
+			break;
+		case "Octubre":
+			alert("Ya paso el frio, ahora calor!!");
+			break;
+		case "Nobiembre":
+			alert("Ya paso el frio, ahora calor!!");
+			break;
+		case "Diciembre":
+			alert("Ya paso el frio, ahora calor!!");
+	}
+	
 
 
 
