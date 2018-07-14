8c9d718ecb99a4990d43dd4609107bb8db385455
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-14 15:01:35 -0300

    Terminado todos los ejercicios If
    terminados en casa

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,14 @@
 function mostrar()
 {
-//tomo la edad  
+	var edad;
+	var msj;
+	var estadoCivil;
 
-	
-
+	edad=document.getElementById('edad').value;
+	estadoCivil=document.getElementById('estadoCivil').value;
+	if (edad<18 && estadoCivil!="Soltero"){
+		msj="Es muy pequeño para no ser soltero";
+		alert(msj);
+	}
 
 }//FIN DE LA FUNCIÓN
