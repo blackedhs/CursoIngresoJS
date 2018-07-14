6ba4913be6be109520fa037aee673fa61a3f7bd0
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-13 22:13:01 -0300

    ferrrete iluminacion
    resuelto con Switch

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -25,7 +25,7 @@ function CalcularPrecio ()
  	cantidadDeLamparas=document.getElementById('Cantidad').value;
  	cantidadDeLamparas=parseInt(cantidadDeLamparas);
  	marca=document.getElementById('Marca').value;
-
+/*
  	if (cantidadDeLamparas>=6)
 	{
 		descuento=50;
@@ -71,6 +71,61 @@ function CalcularPrecio ()
  		}
  	}
  	descuento=(100-descuento)/100;
+ 	precioConDescuento=(precio*cantidadDeLamparas)*descuento;
+ 	if (precioConDescuento>120)
+ 	{
+ 		iibb=precioConDescuento*10/100;
+ 		precioConDescuento=precioConDescuento+iibb;
+ 		document.getElementById('precioDescuento').value=precioConDescuento;
+ 		alert("Usted pago de iibb "+iibb);
+ 	}
+ 	else
+ 	{
+ 		document.getElementById('precioDescuento').value=precioConDescuento;	
+ 	}
+*/
+	switch (cantidadDeLamparas){
+		case 5:
+			switch(marca){
+				case "ArgentinaLuz":
+					descuento=40;
+					break;
+				default: 
+					descuento=30;
+					break; 	
+			}
+			break;
+		case 4:
+			switch (marca){
+				case "ArgentinaLuz":
+				case "FelipeLamparas":
+					descuento=25;
+					break;
+				default:
+					descuento=20;
+					break;
+			}
+			break;
+		case 3:
+			switch(marca){
+				case "ArgentinaLuz":
+					descuento=15;
+					break;
+				case "FelipeLamparas":
+					descuento=10;
+					break;
+				default:
+					descuento=5;
+			}
+			break;
+		case 2:
+		case 1:
+			descuento=0;
+			break;
+		default:
+			descuento=50;
+	}
+	descuento=(100-descuento)/100;
  	precioConDescuento=(precio*cantidadDeLamparas)*descuento;
  	if (precioConDescuento>120)
  	{
