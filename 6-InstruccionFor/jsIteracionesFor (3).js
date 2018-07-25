98d0dc4e3ec43d2b895d64d747e439c89e6540e2
blackedhs <39863970+blackedhs@users.noreply.github.com>
2018-07-24 21:56:25 -0300

    hasta el 4 resuelto

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,11 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	var repeticiones;
 
-
+	repeticiones= prompt("ingrese el número de repeticiones");
+	for (var i = 0; i != repeticiones; i++) {
+		alert("Hola UTN FRA");
+	}
 
 }//FIN DE LA FUNCIÓN
